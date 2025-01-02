c08e95475e8c86f7d943d549b7e2e9f1faafcd64
ERP-FTW <124227412+ERP-FTW@users.noreply.github.com>
2025-01-02 11:10:36 -0500

    pricing, license, image/icon, readme,

--- mlr_ecommerce_nowpayments_2/__manifest__.py
+++ mlr_ecommerce_nowpayments_2/__manifest__.py
@@ -1,11 +1,12 @@
 {
-    "name": "MLR Ecommerce Now Payments 2",
-    "summary": "MLR Ecommerce Now Payments 2",
-    "author": "ERP",
+    "name": "MLR Ecommerce Now Payments",
+    "summary": "MLR Ecommerce Now Payments",
+    "author": "MLR - MI Lightning Rod",
     "website": "https://www.milightningrod.com",
     "category": "Ecommerce",
-    "version": "1.0",
+    "version": "16.0",
     "depends": ["website", "mlr_ecommerce_cryptopayments"],
+    'images': ['static/description/icon.png'],
     "data": [
         "views/now_payment_template.xml",
         "data/now_payment_provider_data.xml",
@@ -18,5 +19,7 @@
     "auto_install": False,
     'post_init_hook': 'post_init_hook',
     'uninstall_hook': 'uninstall_hook',
-    "license": "OPL-1",
+    "license": "LGPL-3",
+    "price": "50.00",
+    "currency": "USD",
 }
