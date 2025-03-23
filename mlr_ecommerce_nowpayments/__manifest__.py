18a55579a925b92175f01a3ff473581082600d2e
ERP-FTW <124227412+ERP-FTW@users.noreply.github.com>
2025-03-22 20:59:32 -0400

    Update __manifest__.py

--- mlr_ecommerce_nowpayments/__manifest__.py
+++ mlr_ecommerce_nowpayments/__manifest__.py
@@ -10,6 +10,7 @@
     "data": [
         "views/now_payment_template.xml",
         "data/now_payment_provider_data.xml",
+        "data/now_payment_method_data.xml",
         "views/now_payment_form.xml",
         #"data/now_payment_icons.xml",
         "views/now_payment_provider.xml",
