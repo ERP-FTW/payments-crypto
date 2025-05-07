c2b7b20e04b2266fd5e6fc5b0a8c8eb8932cb835
Sagars209 <sagars209@gmail.com>
2025-05-07 10:49:44 +0530

    Update __init__.py
    fixed __init__.py to apply v17 changes

--- mlr_ecommerce_nowpayments/__init__.py
+++ mlr_ecommerce_nowpayments/__init__.py
@@ -4,9 +4,10 @@ from . import controllers
 from odoo.addons.payment import setup_provider, reset_payment_provider
 
 
-def post_init_hook(env, registry):
-    setup_provider(env.cr, registry, 'now')
+def post_init_hook(env):
+    setup_provider(env, 'now')
 
 
-def uninstall_hook(env, registry):
-    reset_payment_provider(env.cr, registry, 'now')
+def uninstall_hook(env):
+    reset_payment_provider(env, 'now')
+
